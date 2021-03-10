69e2b5ab85be940da66416273008011fafabe94c
zerinriz <zerin_95@hotmail.com>
2021-03-10 16:57:23 +0100

    Drugi zadatak

---
+++ ex02/src/index.js
@@ -0,0 +1,11 @@
+function main () {
+    // This is a single line JavaScript comment
+    /* document.write ("This is a multi-line comment!");
+    document.write ("This is a multi-line comment!");
+    document.write ("This is a multi-line comment!");
+    document.write ("This is a multi-line comment!");
+    document.write ("This is a multi-line comment!");
+    */
+}
+main();
+module.exports = main;
